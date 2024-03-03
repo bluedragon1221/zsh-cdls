bd5ba52d142ed3d7eab0e5101fa79f17d3369a12
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2024-03-03 12:19:49 -0600

    just call ls. expect user to set their own ls alias

--- zsh-cdls.plugin.zsh
+++ zsh-cdls.plugin.zsh
@@ -3,20 +3,19 @@ chpwd_functions+=(cd_do_ls)
 
 # Since it's a function, we can call it normally, or treat it as a chpwd hook
 cd_do_ls() {
-    if [[ $1 ]]; then
-	clear
-	# ls -A1F --color=always --group-directories-first
-	cd $1
-	eza --oneline --icons -FA --group-directories-first --ignore-glob=".git*" "$1"
-    else
-	clear
-	# ls -A1F --color=always --group-directories-first
-	eza --oneline --icons -FA --group-directories-first --ignore-glob=".git*"
-    fi
+  if [[ $1 ]]; then
+	  clear
+	  cd $1
+	  ls "$1"
+  else
+	  clear
+    ls
+  fi
 }
 
 alias ls="cd_do_ls"
 alias clear="cd_do_ls"
+alias cd="cd_do_ls"
 
 # start the terminal by clearing
 clear
