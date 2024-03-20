b679a84da8627e55a1814d2f456d322a13f978a6
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2024-03-20 14:18:31 -0500

    some changes. simpler, doesn't do more than you want it to

--- zsh-cdls.plugin.zsh
+++ zsh-cdls.plugin.zsh
@@ -1,21 +1,12 @@
 #!/bin/zsh
 chpwd_functions+=(cd_do_ls)
 
-# Since it's a function, we can call it normally, or treat it as a chpwd hook
 cd_do_ls() {
-  if [[ $1 ]]; then
-	  clear
-	  cd $1
-	  ls "$1"
-  else
-	  clear
-    ls
-  fi
+  clear
+  ls
 }
 
-alias ls="cd_do_ls"
-alias clear="cd_do_ls"
-alias cd="cd_do_ls"
+alias clear="clear; ls"
 
-# start the terminal by clearing
-clear
+# start the terminal by ls
+ls
