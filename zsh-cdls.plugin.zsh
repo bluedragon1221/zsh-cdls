6bc0b8a2594813926647c66f6d9119e27025e6e9
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2024-04-01 07:13:39 -0500

    override some commands to clear after they run

--- zsh-cdls.plugin.zsh
+++ zsh-cdls.plugin.zsh
@@ -10,3 +10,23 @@ alias clear="clear; ls"
 
 # start the terminal by ls
 ls
+
+function rm {
+  command rm $@
+  clear
+}
+
+function rmdir {
+  command rmdir $@
+  clear
+}
+
+function touch {
+  command touch $@
+  clear
+}
+
+function mv {
+  command mv $@
+  clear
+}
